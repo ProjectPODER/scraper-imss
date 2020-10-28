6f21c77e76407eae97093ef23f0a27edfabef193
Fernando Matzdorf <fernando@rindecuentas.org>
2020-10-28 15:11:22 -0600

    Added other years back.

--- launch_droplets.sh
+++ launch_droplets.sh
@@ -1,5 +1,5 @@
 #declare -a Years=("1990" "1991" "1992" "1993" "1994" "1995" "1996" "1997" "1998" "1999" "2000" "2001" "2002" "2003" "2004" "2005" "2006" "2007" "2008" "2009" "2010" "2011" "2012" "2013" "2014" "2015" "2016" "2017" "2018" "2019" "2020")
-declare -a Years=("2017")
+declare -a Years=("2017" "2018" "2019" "2020")
 SCRIPT_PATH_REMOTE=/home/nodejs/scrapper-imss
 SCRIPT_PATH_LOCAL=`pwd`/*
 SCRIPT_LABEL=imscrap
