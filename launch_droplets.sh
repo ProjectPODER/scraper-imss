50b5ee3a9fe49800224e1c8da7f77e0d9391af1e
Fernando Matzdorf <fernando@rindecuentas.org>
2020-10-28 15:10:51 -0600

    Fixed bug in launch script preventing browser from launching correctly.

--- launch_droplets.sh
+++ launch_droplets.sh
@@ -1,9 +1,9 @@
 #declare -a Years=("1990" "1991" "1992" "1993" "1994" "1995" "1996" "1997" "1998" "1999" "2000" "2001" "2002" "2003" "2004" "2005" "2006" "2007" "2008" "2009" "2010" "2011" "2012" "2013" "2014" "2015" "2016" "2017" "2018" "2019" "2020")
-declare -a Years=("2017" "2018" "2019" "2020")
+declare -a Years=("2017")
 SCRIPT_PATH_REMOTE=/home/nodejs/scrapper-imss
 SCRIPT_PATH_LOCAL=`pwd`/*
 SCRIPT_LABEL=imscrap
-SCRIPT_DEPENDENCIES=ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
+SCRIPT_DEPENDENCIES="ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils"
 DROPLET_REGION=sfo2
 DROPLET_SIZE=s-1vcpu-1gb
 DROPLET_IMAGE=nodejs-20-04
@@ -24,10 +24,10 @@ for YEAR in ${Years[@]}; do
     ssh nodejs@${IP} "cd $SCRIPT_PATH_REMOTE && npm install"
 
     echo "Launching ${YEAR} in ${IP} ..."
-    ssh nodejs@${IP} screen -d -m -S ${SCRIPT_LABEL} "node ${SCRIPT_PATH_REMOTE}/index.js -o file -y ${YEAR}"
+    ssh nodejs@${IP} screen -d -m -S ${SCRIPT_LABEL} "node ${SCRIPT_PATH_REMOTE}/index.js -v -o file -y ${YEAR}"
 
     echo "Remote screens list in ${IP}"
-    ssh nodejs@${IP} screen -ls 
+    ssh nodejs@${IP} screen -ls
 done;
 
 doctl compute droplet list --tag-name imscrap --format Name,PublicIPv4
